beedc059f5098837717af6fbac6afd84f0a9d9d2
RENATO CARLOS DA SILVA <petrorenato@gmail.com>
2023-08-04 17:24:24 -0300

    Update 01-install.sh

--- Debian Openbox/01-install.sh
+++ Debian Openbox/01-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-apt update && apt upgrade
-apt install -y sudo openbox obconf lxpanel xcompmgr nitrogen dconf-editor thunar lxtask gparted xfce4-terminal lightdm-gtk-greeter-settings grub-customizer synaptic lxrandr zram-tools tlp tlp-rdw
+sudo apt update && apt upgrade
+sudo apt install -y sudo openbox obconf lxpanel xcompmgr nitrogen dconf-editor thunar lxtask gparted xfce4-terminal lightdm-gtk-greeter-settings grub-customizer synaptic lxrandr zram-tools tlp tlp-rdw
