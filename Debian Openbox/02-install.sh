64b6685dbfb3c4ffef5b2c4cfdad4e33e3902c89
RENATO CARLOS DA SILVA <petrorenato@gmail.com>
2023-08-23 18:53:54 -0300

    Update 02-install.sh

--- Debian Openbox/02-install.sh
+++ Debian Openbox/02-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt update && sudo apt upgrade
-sudo apt install --no-install-recommends gufw gnome-calculator gnome-screenshot libreoffice-calc libreoffice-draw libreoffice-impress libreoffice-math libreoffice-writer lximage-qt shotwell gimp pavucontrol imagination mpv vlc winff xfce4-notifyd lxappearance lxinput  
+sudo apt install --no-install-recommends gufw gnome-calculator gnome-screenshot libreoffice-calc libreoffice-draw libreoffice-impress libreoffice-math libreoffice-writer lximage-qt shotwell gimp pavucontrol imagination mpv vlc winff lxappearance lxinput  
